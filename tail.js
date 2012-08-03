838432da5250150c6ec7f63f94214f70d5b6a868
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2012-08-03 14:11:03 +0200

    parametrized connection

--- tail.js
+++ tail.js
@@ -4,13 +4,15 @@ var amqp = require('amqp'),
 	xml2js = require('xml2js'),
 	argv = require('optimist')
 		.usage('Subscribe to AMQP exchange and output messages to the console')
-		.demand('exchange').alias('exchange', 'x').describe('exchange', 'Exchange name')
-		.alias('routingkey', 'r').describe('routingkey', 'Routing key').default({ routingkey: '#' })
+		.demand('x').describe('x', 'Exchange name')
+		.describe('c', 'The connection string')
+		.describe('r', 'Routing key')
+		.default({ r: '#', c: 'amqp://guest:guest@localhost:5672' })
 		.argv;
 
-amqp.createConnection({url: "amqp://guest:guest@localhost:5672"}, null, function (connection) {
+amqp.createConnection({url: argv.c}, null, function (connection) {
 	connection.queue('tail', { exclusive: true }, function (queue) {
-		queue.bind(argv.exchange, argv.routingkey);
+		queue.bind(argv.x, argv.r);
 		queue.subscribe(function (messageBytes, headers, deliveryInfo) {
 			var parser = new xml2js.Parser({ explicitArray: true, mergeAttrs: true }),
 				messageString = messageBytes.data.toString(messageBytes.contentEncoding || 'utf-8');
